5d811d75db2e4fb2d14d7b10500524076a6d9102
Leila Verpoorten <leila.verpoorten@uclouvain.be>
2016-06-17 11:23:10 +0200

    Mise en place du layout sub-layout

--- admission/static/js/creative.js
+++ admission/static/js/creative.js
@@ -9,7 +9,6 @@
 
     // jQuery for page scrolling feature - requires jQuery Easing plugin
     $('a.page-scroll').bind('click', function(event) {
-        alert('kk');
         var $anchor = $(this);
         $('html, body').stop().animate({
             scrollTop: ($($anchor.attr('href')).offset().top - 50)
